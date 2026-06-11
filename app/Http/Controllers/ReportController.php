42c56be64c3677f485756193c720304abcbf4fa9
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-06-11 11:08:49 -0500

    cambios

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -12,6 +12,12 @@ class ReportController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(Request $request)
     {
         if (!auth()->check() || auth()->user()->role_id != 3) {
--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\DB;
 
 class TicketController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function store(Request $request)
     {
         DB::beginTransaction();
